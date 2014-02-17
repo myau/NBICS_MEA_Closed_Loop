f89fc1206ea179f3b1a787e9ab1f9208cfaa2d7d
mayu <bedin.aleksei@yandex.ru>
2014-02-17 15:42:31 +0400

    Delegate tree test

---
+++ MEAClosedLoop/CStatCalc.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+using MEAClosedLoop;
+using System.Threading;
+namespace MEAClosedLoop
+{
+  #region Definitions
+  using TData = System.Double;
+  using TTime = System.UInt64;
+  using TStimIndex = System.Int16;
+  using TAbsStimIndex = System.UInt64;
+  using TRawData = UInt16;
+  using TRawDataPacket = Dictionary<int, ushort[]>;
+  using TFltDataPacket = Dictionary<int, System.Double[]>;
+  #endregion
+  public class CStatCalc
+  {
+    public void StatCalcConsumer(TFltDataPacket data) //it works!
+    {
+      //TODO: calc stat here
+      
+      //TODO: The great logic will be here
+    }
+  }
+}
